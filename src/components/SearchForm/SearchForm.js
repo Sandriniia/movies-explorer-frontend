7972592bc9ef59bc1e6b646ad9a596b0338c7818
Sandra <sandriniia_grebentcova@riseup.net>
2021-08-05 14:47:20 +0200

    change SearchForm component to match the layout

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import './SearchForm.css';
-import loupe from '../../images/find.png';
+import search_button from '../../images/find.png';
 import short_films_on from '../../images/short-films-on.png';
 import short_films_off from '../../images/short-films-off.png';
 
@@ -14,29 +14,28 @@ function SearchForm() {
         <form className='search-form__form'>
           <input className='search-form__input' type='text' autoFocus placeholder='Фильм' />
           <button type='submit' className='search-form__button'>
-            <img src={loupe} alt='loupe icon' className='search-form__icon' />
+            <img src={search_button} alt='loupe icon' className='search-form__icon' />
           </button>
         </form>
-        <div className='search-form__short-films-box'>
-          <p className='search-form__text'>Короткометражки</p>
-          <button className='search-form__short-films-button'>
-            {buttonOn ? (
-              <img
-                src={short_films_on}
-                alt='Short films button on'
-                className='search-form__short-films-icon'
-              />
-            ) : (
-              <img
-                src={short_films_off}
-                alt='Short films button off'
-                className='search-form__short-films-icon'
-              />
-            )}
-          </button>
-        </div>
       </div>
-      <div className='search-form__line'></div>
+      <div className='search-form__short-films-box'>
+        <button className='search-form__short-films-button'>
+          {buttonOn ? (
+            <img
+              src={short_films_on}
+              alt='Short films button on'
+              className='search-form__short-films-icon'
+            />
+          ) : (
+            <img
+              src={short_films_off}
+              alt='Short films button off'
+              className='search-form__short-films-icon'
+            />
+          )}
+        </button>
+        <p className='search-form__text'>Короткометражки</p>
+      </div>
     </section>
   );
 }
